9ab535708fa1aa5961d75e1d79a18d204dfd6f98
Doctuh <kkarlsson0@gmail.com>
2021-12-15 11:45:09 +0100

    added tables in databases

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
+  get 'admin/workers/all', to: 'admin#workers_index'
+  get 'admin/workers/new', to: 'admin#workers_new'
+  post 'admin/workers/new', to: 'admin#workers_create'
+
+  delete 'admin/worker/delete', to: 'admin#worker_delete'
+  get 'admin/shifts', to: 'admin#shifts_index'
+  get 'admin/shifts/search', to: 'admin#shifts_search'
+  get 'admin/shifts/show', to: 'admin#shifts_show'
+
+  post 'shifts/new', to: 'shifts#create'
+
+
 end
--- db/migrate/20211215103735_create_shifts.rb
+++ db/migrate/20211215103735_create_shifts.rb
@@ -1,8 +1,17 @@
 class CreateShifts < ActiveRecord::Migration[6.1]
   def change
     create_table :shifts do |t|
-
       t.timestamps
+
+      t.integer :worker_id
+      t.string :check_in_date
+      t.string :check_in_time
+      t.string :check_out_date
+      t.string :check_out_time
+      t.string :check_in_weekday
+      t.string :check_out_weekday
+      t.date :check_in_full_date
+      t.date :check_out_full_date
     end
   end
 end
